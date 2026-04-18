dcefbb4f47f971996e569a507b70be407d4626fd
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-04-18 09:35:06 +0600

    18-04-2026

--- Programming/C & C++/CodeForces/B_Swap_and_Delete.cpp
+++ Programming/C & C++/CodeForces/B_Swap_and_Delete.cpp
@@ -9,22 +9,80 @@ int main() {
         string s;
         cin >> s;
 
-        int cnt[2] = {0, 0};
+        string t = s;
 
-        // Count number of 0s and 1s
-        for(char c : s) {
-            cnt[c - '0']++;
-        }
+        
+        for(int i=0; i<s.size(); i++){
 
-        for(int i = 0; i <= s.size(); i++) {
+            int flag = 0;
+            if(s[i] == t[i]){
 
-            if(i == s.size() || cnt[1 - (s[i] - '0')] == 0) {
-                cout << s.size() - i << endl;
-                break;
+                if(t[i] == '0'){
+                    for(int j=i+1; j<s.size(); j++){ 
+                    
+                        if(t[j] == '1'){
+                            swap(t[i],t[j]);
+                            flag++;
+                            break;
+                        }
+
+                        if(flag == 0 && j == s.size()){ // jodi khuje na pai?
+                            cout << (((t.length())-i)+1) << endl;
+                        }
+                    }
+                }
+
+                else{
+                    for(int j=i+1; j<s.size(); j++){ 
+                    
+                        if(t[j] == '0'){
+                            swap(t[i],t[j]);
+                            flag++;
+                            break;
+                        }
+
+                        if(flag == 0 && j == s.size()){ // jodi khuje na pai?
+                            cout << (((t.length())-i)+1) << endl;
+                        }
+                    }
+                }
             }
 
-            cnt[1 - (s[i] - '0')]--;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int cnt[2] = {0, 0};
+
+        // // Count number of 0s and 1s
+        // for(char c : s) {
+        //     cnt[c - '0']++;
+        // }
+
+        // for(int i = 0; i <= s.size(); i++) {
+
+        //     if(i == s.size() || cnt[1 - (s[i] - '0')] == 0) {
+        //         cout << s.size() - i << endl;
+        //         break;
+        //     }
+
+        //     cnt[1 - (s[i] - '0')]--;
+        // }
     }
 
     return 0;
