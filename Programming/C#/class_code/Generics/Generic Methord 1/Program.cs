d9d521198c4aafd997be6716a4c5de4afc73247b
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-06-25 18:29:39 +0600

    25-06-2026 ( Generic in c# )

---
+++ Programming/C#/class_code/Generics/Generic Methord 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.CompilerServices;
+
+public class Test
+{
+    public static void print(int[] numbers)
+    {
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static void print(string[] names)
+    {
+        foreach (string i in names)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+
+    public static void Display<A>(A[] names)
+    {
+        foreach (A i in names)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static bool check<B> (B a,B b)
+    {
+        bool c = a.Equals(b);
+        return c;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4 };
+        Test.print(numbers);
+
+        string[] names = { "Korim", "Joim", "Rohim" };
+        Test.print(names);
+
+        Console.WriteLine();
+        Test.Display<int>(numbers);
+        Test.Display<string>(names);
+        Console.WriteLine();
+
+        Console.WriteLine(Test.check(2,2));
+        Console.WriteLine(Test.check("ali", "alic"));
+    }
+}
---
+++ Programming/C#/class_code/Generics/Generic class 1/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Student <T>
+{
+    private T name { get; set; }
+    private T id { get; set; }
+
+    private T age { get; set; }
+
+    public Student(T name, T id, T age  )
+    {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+
+    public T Display()
+    {
+        Console.WriteLine($"Name => {name} | Age => {age}");
+        return id;
+    }
+}
+
+
+
+public class Mainn
+{
+    public static void Main(string[] args)
+    {
+        Student<int> s = new Student<int>(1, 2, 3);
+        int ss = s.Display();
+        Console.WriteLine(ss);
+
+        Student<string> s2 = new Student<string>("1", "2", "3");
+        string ss2 = s2.Display();
+        Console.WriteLine(ss2);
+
+        Console.ReadKey();
+    }
+}
