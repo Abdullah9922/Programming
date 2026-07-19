b20f357beed1e25b8f56ec5d8fa7aa46d56f1f98
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-07-19 11:18:18 +0600

    19-07-2026 ( Problem solving )

---
+++ Programming/C#/Problem Solving/A. Bigrams/Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class Solution
+{
+    public static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        while (t-- > 0)
+        {
+            int k = int.Parse(Console.ReadLine());
+
+            int[] a = Console.ReadLine()
+                             .Split()
+                             .Select(int.Parse)
+                             .ToArray();
+
+            int maxValue = a.Max();
+            int countTwo = a.Count(x => x == 2);
+
+            if (maxValue > 2 || countTwo > 1)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
---
+++ Programming/C#/Problem Solving/Codeforces Round 1111 (Div. 2)/Problem A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main()
+    {
+        int tc = int.Parse(Console.ReadLine());
+
+        while (tc-- > 0)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split();
+
+            int x = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (int.Parse(input[i]) == -1)
+                    x++;
+            }
+
+            if (n % 2 != 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine((x % 2 == (n / 2) % 2) ? "YES" : "NO");
+            }
+        }
+    }
+}
