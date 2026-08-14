45df3f2cbd3109cd06617ede20c5a8840913552a
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-08-14 10:29:42 +0600

    14-08-2026

---
+++ Programming/C#/practice_code/A. MEX Partition/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int tc = int.Parse(Console.ReadLine());
+
+        int n;
+        while (tc-- > 0)
+        {
+            n = int.Parse(Console.ReadLine());
+
+            var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            int max = arr.Max();
+            int mex = 0;
+            while (true)
+            {
+                bool contain = arr.Contains(mex);
+                if (!contain) break;
+                mex++;
+                
+            }
+            Console.WriteLine(mex);
+        }
+    }
+}
