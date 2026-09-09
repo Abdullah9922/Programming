a2cf64d682a1539d9c4fc070b8f42124d8a019ea
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-09-09 13:56:28 +0600

    09-09-2026

---
+++ Programming/C#/Problem Solving/B. Tournament/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int tc = int.Parse(Console.ReadLine());
+
+int n, j, k;
+while (tc-- > 0)
+{
+    string[] input = Console.ReadLine().Split();
+    n = int.Parse(input[0]);
+    j = int.Parse(input[1]);
+    k = int.Parse(input[2]);
+
+    var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+    int mx = arr.Max();
+    if (k > 1 || arr[j-1] == mx)
+        Console.WriteLine("YES");
+    else
+        Console.WriteLine("NO");
+
+
+}
