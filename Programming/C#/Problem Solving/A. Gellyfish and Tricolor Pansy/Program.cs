e38f63c4d7c88d8134f4b463adb40445a1a90929
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-09-20 12:03:30 +0600

    Problem Solving\A. Gellyfish and Tricolor Pansy

---
+++ Programming/C#/Problem Solving/A. Gellyfish and Tricolor Pansy/Program.cs
@@ -0,0 +1,51 @@
+﻿
+int tc = int.Parse(Console.ReadLine());
+
+int a, b, c, d;
+while (tc-- > 0)
+{
+    string[] input = Console.ReadLine().Split();
+    a = int.Parse(input[0]);
+    b = int.Parse(input[1]);
+    c = int.Parse(input[2]);
+    d = int.Parse(input[3]);
+
+
+
+    if (Math.Min(a, c) >= Math.Min(b, d)) Console.WriteLine("Gellyfish");
+    else Console.WriteLine("Flower");
+
+
+
+    //int k = 1;
+    //while (true)
+    //{
+    //    if (k % 2 != 0 && c > 0)
+    //    {
+    //        if (b < d) b--;
+    //        else d--;
+    //    }
+
+    //    else if (k % 2 == 0 && d > 0)
+    //    {
+
+    //         if (a < c) a--;
+    //         else c--;
+
+    //    }
+
+    //    if (b == 0 || d == 0)
+    //    {
+    //        Console.WriteLine("Gellyfish");
+    //        break;
+    //    }
+
+    //    else if (a == 0 || c == 0)
+    //    {
+    //        Console.WriteLine("Flower");
+    //        break;
+    //    }
+    //    k++;
+
+    //}
+}
