05f4e685b79a464b298d45ef4c4e094748b9922e
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-09-21 09:26:35 +0600

    A. Square Year

---
+++ Programming/C#/Problem Solving/A. Square Year/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int tc = int.Parse(Console.ReadLine());
+
+int s;
+while (tc-- > 0)
+{
+    s = int.Parse( Console.ReadLine());
+
+    double root = Math.Sqrt(s);
+
+    if (root == (int)root)
+    {
+        Console.WriteLine(0 + " " + root);
+    }
+    else Console.WriteLine(-1);
+}
